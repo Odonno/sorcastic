a6ecf410c9c8183f6752096bda634762114be1dd
Odonno <bottiau.david@laposte.net>
2021-10-19 13:47:53 +0200

    ✨ create index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="bg-black text-gray-100 h-screen flex justify-center items-center flex-col">
+	<q class="text-3xl italic">
+		To know, is to know that you know nothing. That is the meaning of true knowledge.
+	</q>
+
+	<div class="mt-4 text-gray-400 flex text-xl">
+		<span>Socrates</span>
+		<span>, 469 BC - 399 BC</span>
+	</div>
+</section>
